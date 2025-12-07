e25c16aaa2eb74f3b79353492d6eea225e9c06fe
Ezan Obaid <ezaanubaid@gmail.com>
2025-12-07 13:55:12 +0500

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>React + Node App Running</h1>
+    </div>
+  );
+}
+
+export default App;
+
+  
